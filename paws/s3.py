69a08f0313781990966c49db8ebd76f5e65ace3a
Mike Kane <guido.vanrawesome@gmail.com>
2020-03-30 01:02:55 -0700

    create s3 file and tests

--- paws/s3.py
+++ paws/s3.py
@@ -1,5 +1,5 @@
 """
-S3 methods for PAWS library
+S# methods for PAWS library
 """
 
 import boto3
@@ -7,9 +7,8 @@ from sensible.loginit import logger
 
 log = logger("Paws")
 
-
 def boto_s3_resource():
-    """Create boto3 S3 Resource"""
+    """Create boto3 s3 Resource"""
 
     return boto3.resource("s3")
 
@@ -18,8 +17,8 @@ def download(bucket, key, filename, resource=None):
 
     if resource is None:
         resource = boto_s3_resource()
-    log_msg = "Attempting download: {bucket}, {key}, {filename}".\
-        format(bucket=bucket, key=key, filename=filename)
+    log_msg = "Attempting download: {}, {}, {}".format(bucket, key, filename)
     log.info(log_msg)
     resource.meta.client.download_file(bucket, key, filename)
+    
     return filename
--- tests/test_s3.py
+++ tests/test_s3.py
@@ -3,16 +3,16 @@ import boto3
 from moto import mock_s3
 from paws.s3 import download
 
-@pytest.yield_fixture(scope="session")
+@pytest.yield_fixture(scope='session')
 def mock_boto():
-    """Setup Mock Objects"""
-    
+    """Setup mock objects"""
+
     mock_s3().start()
     output_str = 'Something'
     resource = boto3.resource('s3')
-    resource.create_bucket(Bucket="gdelt-open-data")
-    resource.Bucket("gdelt-open-data").put_object(Key="events/1979.csv",
-                                                  Body=output_str)
+    resource.create_bucket(Bucket='gdelt-open-data')
+    resource.Bucket('gdelt-open-data').put_object(Key='events/1979.csv', Body=output_str)
+
     yield resource
     mock_s3().stop()
 
@@ -20,9 +20,6 @@ def test_download(mock_boto):
     """Test s3 download function"""
 
     resource = mock_boto
-    res = download(resource=resource, bucket="gdelt-open-data",
-                   key="events/1979.csv", filename="1979.csv")
-    assert res == "1979.csv"
-    
-    import os
-    os.unlink("1979.csv")
+    res = download(resource=resource, bucket='gdelt-open-data', key='events/1979.csv', filename='1979.csv')
+
+    assert res == '1979.csv'
